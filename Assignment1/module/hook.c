0ff35b223b073ceee0ca4c5870c0a878ad4beaad
Krishna Tayal <krishnatayal111@gmail.com>
2019-02-17 22:53:31 +0530

    Can be dilivered without q2

--- Assignment1/module/hook.c
+++ Assignment1/module/hook.c
@@ -2,8 +2,8 @@
 #include "interface.h"
 
 #include <asm/traps.h>
-//#include "../../../../kernelFile/linux-4.19.13/arch/x86/entry/calling.h"
 
+#define TEST
 static int command;
 static unsigned long tlb_misses, readwss, writewss, unused;
 
@@ -47,12 +47,16 @@ static ssize_t memtrack_command_set(struct kobject *kobj,
 	command = buf[0] - '0';
 	if(command < 0 || command > 2)
 	{
+		#ifdef TEST
 		printk(KERN_INFO "In %s, Invalid command = [%d]", __FUNCTION__, command);
+		#endif
 		return 0;
 	}
 	else
 	{
+		#ifdef TEST
 		printk(KERN_INFO "In %s, command = [%d]", __FUNCTION__, command);
+		#endif
         	return count;
 	}
 }
@@ -109,9 +113,16 @@ void user_cr3(void)
                               :"memory");
 
 
-               printk(KERN_INFO "current cr3 = %lx\n", cr3);
+		#ifdef TEST
+		printk(KERN_INFO "current cr3 = %lx\n", cr3);
+		#endif
+
 		cr3 |= (0x1UL << 11);
-               printk(KERN_INFO "now cr3 = %lx\n", cr3);
+
+		#ifdef TEST
+		printk(KERN_INFO "now cr3 = %lx\n", cr3);
+		#endif
+
         	__asm__ __volatile__("mov %0, %%cr3;"
 			      :
                               :"r" (cr3)
@@ -125,10 +136,16 @@ void kernel_cr3(void)
 			      :
                               :"memory");
 
-               printk(KERN_INFO "kernel current cr3 = %lx\n", cr3);
+		#ifdef TEST
+		printk(KERN_INFO "kernel current cr3 = %lx\n", cr3);
+		#endif
 
 		cr3 &= ~(0x1UL << 11);
-               printk(KERN_INFO "kernel now cr3 = %lx\n", cr3);
+
+		#ifdef TEST
+		printk(KERN_INFO "kernel now cr3 = %lx\n", cr3);
+		#endif
+
         	__asm__ __volatile__("mov %0, %%cr3;"
 			      :
                               :"r" (cr3)
@@ -145,7 +162,9 @@ static pte_t* get_pte(unsigned long address, unsigned long *addr_vma)
         struct mm_struct *mm = current->mm;
         struct vm_area_struct *vma = find_vma(mm, address);
         if(!vma){
+		#ifdef TEST
                  printk(KERN_INFO "No vma yet\n");
+		 #endif
                  goto nul_ret;
         }
        
@@ -154,7 +173,9 @@ static pte_t* get_pte(unsigned long address, unsigned long *addr_vma)
         pgd = pgd_offset(mm, address);
         if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd)))
                 goto nul_ret;
+	#ifdef TEST
         printk(KERN_INFO "pgd(va) [%lx] pgd (pa) [%lx] *pgd [%lx]\n", (unsigned long)pgd, __pa(pgd), pgd->pgd); 
+	#endif
         p4d = p4d_offset(pgd, address);
         if (p4d_none(*p4d))
                 goto nul_ret;
@@ -165,89 +186,96 @@ static pte_t* get_pte(unsigned long address, unsigned long *addr_vma)
                 goto nul_ret;
         if (unlikely(pud_bad(*pud)))
                 goto nul_ret;
+	#ifdef TEST
         printk(KERN_INFO "pud(va) [%lx] pud (pa) [%lx] *pud [%lx]\n", (unsigned long)pud, __pa(pud), pud->pud); 
+	#endif
 
         pmd = pmd_offset(pud, address);
         if (pmd_none(*pmd))
                 goto nul_ret;
         if (unlikely(pmd_trans_huge(*pmd))){
+		#ifdef TEST
                 printk(KERN_INFO "I am huge\n");
+		#endif
                 goto nul_ret;
         }
+	#ifdef TEST
         printk(KERN_INFO "pmd(va) [%lx] pmd (pa) [%lx] *pmd [%lx]\n", (unsigned long)pmd, __pa(pmd), pmd->pmd); 
+	#endif
         ptep = pte_offset_map(pmd, address);
         if(!ptep){
+		#ifdef TEST
                 printk(KERN_INFO "pte_p is null\n\n");
+		#endif
                 goto nul_ret;
         }
+	#ifdef TEST
         printk(KERN_INFO "pte(va) [%lx] pte (pa) [%lx] *pte [%lx]\n", (unsigned long)ptep, __pa(ptep), ptep->pte); 
+	#endif
         return ptep;
 
         nul_ret:
+		#ifdef TEST
                printk(KERN_INFO "Address could not be translated\n");
+	       #endif
                return NULL;
 
 }
 
-static void poison_code(int flag)
-{
-	int i, num_code_pages;
-	unsigned long addr;
-	unsigned long	start_code = current->mm->start_code;
-	unsigned long	end_code = current->mm->end_code;
-
-	num_code_pages = (end_code - start_code)>>12;
-	for(i = 0; i < num_code_pages; i++)
-	{
-		addr = start_code + (i << 12);
-        	gpte = get_pte(addr, &vma);
-                if(!flag)	gpte->pte |= (0x1UL << 50);
-		else		gpte->pte &= ~(0x1UL << 50);
-	}
-	if(!flag)	printk(KERN_INFO "Code poisoned");
-	else		printk(KERN_INFO "Code Unpoisoned");
-}
 
 static int fault_hook(struct mm_struct *mm, struct pt_regs *regs, unsigned long error_code, unsigned long address)
 {
-	unsigned long tmp;
+	unsigned long tmp = 15;
 	//Hook will work only for given range of address
 	if(((startAddr >> 12) <= (address>>12)) && ((endAddr >> 12) > (address>>12)))
-	 	//&& (error_code & X86_PF_USER) ) 
 	{
 		//Walk through page table to get pte
         	gpte = get_pte(address, &vma);
 
+		if(!gpte)	return -1;
+
 		//Unpoison the entry
                 gpte->pte &= ~(0x1UL << 50);
 
+		if(command == 1)
+		{
+			//Read cr3 and set its 12th bit
+        		__asm__ __volatile__("mov %%cr3, %0;"
+                              :"=r" (cr3)
+			      :
+                              :"memory");
+
+			cr3 |= (0x1UL << 11);
+
+        		__asm__ __volatile__("mov %0, %%cr3;"
+			      :
+                              :"r" (cr3)
+                              :"memory");
+		}
+
 		//Access the page to add its entry to tlb
-		user_cr3();
 		stac();
-                tmp = *(unsigned long *)address;
+                //tmp = *(unsigned long *)address;
 		*(unsigned long *)address = tmp;
 		clac();
-		kernel_cr3();
 
-		//Poison the netry again
+
+		if(command == 1)
+		{
+			//Reset 12th bit
+			cr3 &= ~(0x1UL << 11);
+
+        		__asm__ __volatile__("mov %0, %%cr3;"
+			      :
+                              :"r" (cr3)
+                              :"memory");
+		}
+
+		//Poison the entry again
                 gpte->pte |= (0x1UL << 50);
 
 		//Update all the counters
-                tlb_misses++;
-		//if(command == 2)
-		{
-			int i;
-			readwss = 0;
-			writewss = 0;
-			unused = 0;
-
-			for(i = 0; i < numPages; i++)
-			{
-				if(write_count[i].count > 0)		writewss++;
-				else if(read_count[i].count > 0)	readwss++;
-				else					unused++;
-			}
-		}
+                if(command != 2)	tlb_misses++;
 
 		if(miss_count && read_count && write_count)	
 		{
@@ -257,7 +285,28 @@ static int fault_hook(struct mm_struct *mm, struct pt_regs *regs, unsigned long 
 			else				read_count[idx].count++;
 		}
 
-                printk(KERN_INFO "PAGE Fault COUNT %ld\n", tlb_misses);
+		if(command == 2)
+		{
+			int i;
+			unsigned long rtmp = 0;
+			unsigned long wtmp = 0;
+			unsigned long utmp = 0;
+			for(i = 0; i < numPages; i++)
+			{
+				if(write_count[i].count > 0)		wtmp++;
+				else if(read_count[i].count > 0)	rtmp++;
+				else					utmp++;
+			}
+                        
+			readwss = rtmp;
+			writewss = wtmp;
+			unused = utmp;
+		}
+
+
+		#ifdef TEST
+                printk(KERN_INFO "Page fault till now [%ld]\n", tlb_misses);
+		#endif
                 return 0;
         }
 
@@ -275,6 +324,7 @@ int fill_toppers(struct read_command *cmd, PageInfo *count_ptr)
 		return -1;
 	}
 
+	//Use selection sort to get toppers
 	for(i = 0; i < MAX_TOPPERS && i < numPages; i++)
 	{
 		max_idx = 0;
@@ -303,26 +353,35 @@ ssize_t handle_read(char *buff, size_t length)
 	struct read_command cmd;
 	int i;
 
+	//Size of the data must be correct
 	if(length != sizeof(struct read_command))
         {
+		#ifdef TEST
                 printk(KERN_INFO "In %s. Length = [%ld] is not correct", __FUNCTION__, length);
+		#endif
                 return -1;
         }
 
+	//Read command from user space
 	stac();
 	ptr1 = (struct read_command *)buff;
 	cmd.command = ptr1->command;
 	clac();
 
 	ptr2 = &cmd;
+	#ifdef TEST
 	printk(KERN_INFO "In %s. command is [%ld]", __FUNCTION__, ptr2->command);
+	#endif
 
+	//calculate counters 
 	switch(ptr2->command)
 	{
 		case FAULT_START:
 			page_fault_pid = current->pid;
                 	rsvd_fault_hook = fault_hook;
+			#ifdef TEST
                 	printk(KERN_INFO "In %s. Fault hook and pid is set", __FUNCTION__);
+			#endif
 			return 0;
 
 		case TLBMISS_TOPPERS:
@@ -330,25 +389,30 @@ ssize_t handle_read(char *buff, size_t length)
 			break;
 
 		case READ_TOPPERS:
-			fill_toppers(ptr2, miss_count);
+			fill_toppers(ptr2, read_count);
 			break;
 
 		case WRITE_TOPPERS:
-			fill_toppers(ptr2, miss_count);
+			fill_toppers(ptr2, write_count);
 			break;
 
 		default:
+			#ifdef TEST
 			printk(KERN_INFO "In %s. Invalid command [%ld]", __FUNCTION__, ptr2->command);
+			#endif
                 	return -1;
 	}
 
 	stac();
         ptr1->valid_entries = ptr2->valid_entries;
-	for(i = 0; i < MAX_TOPPERS; i++)	ptr1->toppers[i] = ptr2->toppers[i];
+	for(i = 0; i < MAX_TOPPERS; i++)	
+	{
+		ptr1->toppers[i].vaddr = ptr2->toppers[i].vaddr;
+		ptr1->toppers[i].count = ptr2->toppers[i].count;
+	}
         clac();
-	return 0;
 
-	//printk(KERN_INFO "Process pid [%d] cr3 [%lx]\n", current->pid, cr3);
+	return 0;
 }
 
 static void free_old_map(void)
@@ -363,9 +427,12 @@ static void free_old_map(void)
 	}
 
 	old_map = NULL;
+	#ifdef TEST
         printk(KERN_INFO "In %s. Old map is freed\n", __FUNCTION__);
+	#endif
 }
 
+//Check using oldmap, whether any old map area has been merged to new mmap
 static void check_size(void)
 {
 	struct vm_area_struct *itr = NULL;
@@ -383,7 +450,10 @@ static int find_num_pages(void)
 	//Iterator on current mmap
 	struct vm_area_struct *itr = NULL;
 
+	#ifdef TEST
 	printk(KERN_INFO "In %s", __FUNCTION__);
+	#endif
+
 	//Iterate over current mmap and check where is the user provided address
 	for(itr = current->mm->mmap; itr != NULL; itr = itr->vm_next)
 	{
@@ -401,17 +471,22 @@ static int find_num_pages(void)
 	//No entry found for user provided address
 	if(itr == NULL)	
 	{
+		#ifdef TEST
 		printk(KERN_INFO "In %s. No mmap entry found for startAddr = [%lx]\n", 
 				__FUNCTION__, startAddr);
+		#endif
 		return -1;
 	}
 
 	numPages = (endAddr - startAddr) >> 12; 
+	#ifdef TEST
 	printk(KERN_INFO "In %s. numPages = [%ld], endAddr = [%lx]\n", 
 				__FUNCTION__, numPages, endAddr);
+	#endif
 	return 0;
 }
 
+//Free memory allocated for counters
 void free_page_info(void)
 {
 	if(miss_count)	kfree(miss_count);
@@ -421,6 +496,7 @@ void free_page_info(void)
 	write_count = NULL;
 }
 
+//Malloc memory for counters
 int malloc_page_info(void)
 {
 	int i;
@@ -429,7 +505,9 @@ int malloc_page_info(void)
 	miss_count = kzalloc(sizeof(PageInfo) * numPages * 3, GFP_ATOMIC);
 	if(miss_count == NULL)
 	{
+		#ifdef TEST
 		printk(KERN_INFO "In %s. Cannot allocate memory for miss count\n", __FUNCTION__);
+		#endif
 		return -1;
 	}
 
@@ -455,18 +533,28 @@ ssize_t handle_write(const char *buff, size_t length)
 	//Write is accepting only long type inputs
 	if(length != 8)	
 	{
+		#ifdef TEST
 		printk(KERN_INFO "In %s. Length = [%ld] is not correct", __FUNCTION__, length);
+		#endif
 		return -1;
 	}
 
+	//Read the address provided by user
+	stac();
 	startAddr = *(long *)buff;
+	clac();
+
+	#ifdef TEST
 	printk(KERN_INFO "In %s. startAddr = [%lx]\n", __FUNCTION__, startAddr);
+	#endif
 
 	//Calculate the number of pages in user provided mmap area
 	if(find_num_pages() != 0)
 	{
+		#ifdef TEST
 		printk(KERN_INFO "In %s. No entry in mmap found for startAddr = [%lx]\n", 
 				__FUNCTION__, startAddr);
+		#endif
 		return -1;
 	}
 
@@ -477,18 +565,20 @@ ssize_t handle_write(const char *buff, size_t length)
 	for(i = 0; i < numPages; i++)
 	{
         	gpte = get_pte(ptr, &vma);
-        	gpte->pte |= 0x1UL << 50;
+        	if(gpte)    gpte->pte |= (0x1UL << 50);
 		ptr += (1<<12);
 	}
 
-	//__native_tlb_flush_one_user(gptr);
    	return 0;
 }
 
+//Read the current mmap data strcuture
 static int read_mmap(void)
 {
 	struct vm_area_struct *itr, *ptr, *prev = NULL, *next;
+	#ifdef TEST
 	printk(KERN_INFO "In %s", __FUNCTION__);
+	#endif
 
 	//Loop through current mmap list
 	for(itr = current->mm->mmap; itr != NULL; itr = itr->vm_next)
@@ -510,9 +600,12 @@ static int read_mmap(void)
 	}
 
 
+	//If some alocation failed, then free already allocated memory
 	if(ptr == NULL)
 	{
+		#ifdef TEST
 		printk(KERN_INFO "In %s. Failed to allocate memory.\n", __FUNCTION__); 
+		#endif
 
 		//Free previously allocated memory
 		for(ptr = old_map; ptr != NULL; ptr = next) 
@@ -527,13 +620,28 @@ static int read_mmap(void)
 	return 0;
 }
 
-int handle_open(void)
+void reset_counters(void)
 {
-	printk(KERN_INFO "In %s", __FUNCTION__);
+	//Remove pag fault hook
+	page_fault_pid = -1;
+        rsvd_fault_hook = NULL;
+
 	tlb_misses = 0;
+	writewss = 0;
+	readwss = 0;
+	unused = 0;
 	startAddr = 0;
 	endAddr = 0;
-	user_cr3();
+	numPages = 0;
+	gpte = 0;
+}
+
+int handle_open(void)
+{
+	#ifdef TEST
+	printk(KERN_INFO "In %s", __FUNCTION__);
+	#endif
+	reset_counters();
 	return read_mmap();
 }
 
@@ -542,11 +650,12 @@ int handle_close(void)
 	int i;
 	long ptr;
 
+	#ifdef TEST
 	printk(KERN_INFO "In %s.\n", __FUNCTION__);
+	#endif
 
 	//Free old map if not already freed
 	if(old_map)	free_old_map();
-
 	free_page_info();
 
 	//Unpoison all the user entries
@@ -554,25 +663,13 @@ int handle_close(void)
 	for(i = 0; i < numPages; i++)
 	{
 		gpte = get_pte(ptr, &vma);
-		gpte->pte &= ~(0x1UL << 50);
+		if(gpte)    gpte->pte &= ~(0x1UL << 50);
 		ptr += (1<<12);
 	}
 
 	//Remove pag fault hook
 	page_fault_pid = -1;
         rsvd_fault_hook = NULL;
-
-	//Reset user address info
-	startAddr = 0;
-	endAddr = 0;
-	numPages = 0;
-
-	//Reset counters
-	gpte = 0;
-	tlb_misses = 0;
-	writewss = 0;
-	readwss = 0;
-	unused = 0;
-
+	//reset_counters();
    	return 0;
 }
